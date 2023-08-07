f6c79a0f817485e6c46a3e253205105e473bf89c
Yonghun <yonghuni484@gmail.com>
2023-08-07 11:44:04 +0900

    Config: Add prettierrc.js

---
+++ prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: false, // 큰따옴표를 작은따옴표로 자동수정
+  jsxSingleQuote: false,
+  tabWidth: 2, // 탭너비 2로 설정
+  semi: true, // 세미콜론 자동수정
+  trailingComma: "es5", // 객체나 배열 등 마지막 줄의 콤마 여부(ES5부터 허용)
+  bracketSpacing: true,
+};
